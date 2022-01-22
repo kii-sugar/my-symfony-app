d4128fcce2788130f3da212e291895d8b329e661
NahoKizawa <n.kizawa.629@gmail.com>
2022-01-22 18:04:42 +0900

    action method with path parameter

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+// AbstractController
+// Twigというテンプレートエンジンを利用して画面をレンダリング表示するためにも必要
+class HelloController extends AbstractController
 {
+		// @Roteアノテーションで{値}を指定して、アクションメソッドの引数$変数に渡される
+		// 注意：パスパラメータが無いとエラーになる->デフォルト値を設定しておくとよい
     /**
-    * @Route("/hello", name="hello")
+    * @Route("/hello/{name}/{pass}", name="hello")
     */
-    public function index()
+    public function index($name=('(noname)'), $pass='(no password)')
     {
-        return new Response('Hello Symfony!');
+      $result = '<html><body>';
+			$result .= '<h1>Subscribed Services</h1>';
+			$result .= '<ol>';
+			$result .= '<li>name: ' . $name . '</li>';
+			$result .= '<li>pwd : ' . $pass . '</li>';
+			$result .= '</ol>';
+			$result .= '</body></html>';
+      return new Response($result);
     }
 }
