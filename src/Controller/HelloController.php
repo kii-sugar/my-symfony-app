afad918ef4d64fa230b12d357dc33dd79e0cbf15
NahoKizawa <n.kizawa.629@gmail.com>
2022-01-22 18:23:06 +0900

    redirect

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 // AbstractController
@@ -28,4 +29,16 @@ class HelloController extends AbstractController
 			$result .= '</body></html>';
       return new Response($result);
     }
+
+		/**
+		 * @Route("/other/{domain}", name="other")
+		 */
+		public function other(Request $request, $domain='')
+		{
+			if ($domain == '') {
+				return $this -> redirect('/hello');
+			} else {
+				return new RedirectResponse("https://{$domain}.com");
+			}
+		}
 }
