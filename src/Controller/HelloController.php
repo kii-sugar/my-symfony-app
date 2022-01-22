bc51483dc7e955f510f923c9bc9a40df3175d77f
NahoKizawa <n.kizawa.629@gmail.com>
2022-01-22 18:15:29 +0900

    action method with query oarameter

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,15 +14,16 @@ class HelloController extends AbstractController
 		// @Roteアノテーションで{値}を指定して、アクションメソッドの引数$変数に渡される
 		// 注意：パスパラメータが無いとエラーになる->デフォルト値を設定しておくとよい
     /**
-    * @Route("/hello/{name}/{pass}", name="hello")
+    * @Route("/hello", name="hello")
     */
-    public function index($name=('(noname)'), $pass='(no password)')
+    public function index(Request $request)
     {
+			$name = $request -> get('name');
       $result = '<html><body>';
 			$result .= '<h1>Subscribed Services</h1>';
 			$result .= '<ol>';
 			$result .= '<li>name: ' . $name . '</li>';
-			$result .= '<li>pwd : ' . $pass . '</li>';
+			$result .= '<li>pwd : ' . $request -> get('pass') . '</li>';
 			$result .= '</ol>';
 			$result .= '</body></html>';
       return new Response($result);
