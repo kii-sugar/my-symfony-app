90759638f7d9493a1864b9ffaa37efe88c435e81
NahoKizawa <n.kizawa.629@gmail.com>
2022-01-23 09:50:03 +0900

    xml response

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,29 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Serializer\Serializer; // オブジェクトのシリアライズ(テキストデータに変換する処理)
+use Symfony\Component\Serializer\Encoder\XmlEncoder; // XMLデータの変換
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer; // オブジェクトへの変換
 
 // AbstractController
 // Twigというテンプレートエンジンを利用して画面をレンダリング表示するためにも必要
 class HelloController extends AbstractController
 {
-		// @Roteアノテーションで{値}を指定して、アクションメソッドの引数$変数に渡される
-		// 注意：パスパラメータが無いとエラーになる->デフォルト値を設定しておくとよい
     /**
     * @Route("/hello", name="hello")
     */
     public function index(Request $request)
     {
-			$name = $request -> get('name');
-      $result = '<html><body>';
-			$result .= '<h1>Subscribed Services</h1>';
-			$result .= '<ol>';
-			$result .= '<li>name: ' . $name . '</li>';
-			$result .= '<li>pwd : ' . $request -> get('pass') . '</li>';
-			$result .= '</ol>';
-			$result .= '</body></html>';
-      return new Response($result);
+			$encoders = array(new XmlEncoder());
+			$normalizers = array(new ObjectNormalizer());
+			$serializer = new Serializer($normalizers, $encoders);
+
+			$data = array(
+				'name' => array(
+					'first' => 'Hanako',
+					'second' => 'Tanaka'
+				),
+				'age' => 29
+			);
+
+			$response = new Response();
+			$response->headers->set('Content-Type', 'xml');
+			$result = $serializer->serialize($data, 'xml');
+			$response->setContent($result);
+			return $response;
     }
 
 		/**
