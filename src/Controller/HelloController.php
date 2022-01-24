1fb2aecb4dd710649e5798de0213ddc6e5d81dfc
NahoKizawa <n.kizawa.629@gmail.com>
2022-01-24 14:11:08 +0900

    twig template

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,53 +8,37 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
 // AbstractController
 // Twigというテンプレートエンジンを利用して画面をレンダリング表示するためにも必要
 class HelloController extends AbstractController {
   /**
   * @Route("/hello", name="hello")
   */
-  public function index(Request $req, SessionInterface $session) {
-		$data = new MyData();
+  public function index(Request $req) {
 
-		$form = $this->createFormBuilder($data)
-			->add('data', TextType::class)
-			->add('save', SubmitType::class,[
-				'label' => 'Click!!'
-			])
-			->getForm();
-
-		if ($req->getMethod() == 'POST') {
-			$form->handleRequest($req); // reqをフォームに適合
-			$data = $form->getData(); // フォームのPersonインスタンスを取り出す
-			if ($data->getData() == '!') {
-				$session->remove('data');
-			} else {
-				$session->set('data', $data->getData());
-			}
-		} 
-		return $this->render('hello/index.html.twig', [
-			'title' => 'Hello',
-			'data' => $session->get('data'),
-			'form' => $form->createview()
+		$data = [
+			array(
+				'name' => 'Naho',
+				'age' => 26,
+				'hobby' => 'music, traning',
+				'mail' => 'naho@gmail.com'
+			),
+			array(
+				'name' => 'Akihiro',
+				'age' => 28,
+				'hobby' => 'baseball, game',
+				'mail' => 'akihiro@gmail.com'
+			),array(
+				'name' => 'otochi',
+				'age' => 64,
+				'hobby' => 'cooking',
+				'mail' => 'otochi@gmail.com'
+			)
+			];
+		return $this -> render('hello/index.html.twig', [
+			'title' => 'Ultra',
+			'message' => 'テンプレートで計算を行います',
+			'data' => $data
 		]);
   }
-}
-
-// データクラス
-class MyData {
-	protected $data = '';
-
-	public function getData() {
-		return $this->data;
-	}
-
-	public function setData($data) {
-		$this->data = $data;
-	}
 }
