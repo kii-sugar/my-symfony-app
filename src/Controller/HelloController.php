b20a14eecf910ce9d0f2ee9a88c2b8f0a05d3215
NahoKizawa <n.kizawa.629@gmail.com>
2022-02-01 15:58:54 +0900

    flash message

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Form\PersonType;
+use App\Form\HelloType;
 use App\Entity\Person;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Query\ResultSetMappingBuilder;
@@ -23,25 +27,48 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 // AbstractController
 // Twigというテンプレートエンジンを利用して画面をレンダリング表示するためにも必要
 class HelloController extends AbstractController {
-	private $em;
 
 	public function __construct(private ManagerRegistry $doctrine) {}
 	/**
   * @Route("/hello", name="hello")
   */
-	public function index(Request $req, EntityManagerInterface $em) {
+	public function index(Request $req, SessionInterface $session)
+	{
+		$formobj = new HelloForm();
+		// フラッシュメッセージ
+		$form = $this->createForm(HelloType::class, $formobj);
+		$form->handleRequest($req);
 
-		$this->em = $em;
-
-		$repository = $this->em->getRepository(Person::class);
-		$data = $repository->findall();
+		if( $req->getMethod() == 'POST') {
+			$formobj = $form->getData();
+			$session->getFlashBag()->add('info.mail', $formobj);
+			// info.mailというタイプを指定して好きなオブジェクトを追加する
+			// こちらは普通に変数で渡している
+			$msg = 'Hello, ' . $formobj->getName() . '!!';
+		} else {
+			$msg = 'send form';
+		}
 		
+		$repository = $this->doctrine->getRepository(Person::class);
+		$data = $repository->findall();
+
 		return $this -> render('hello/index.html.twig', [
 			'title' => 'Ultra',
-			'message' => 'データベースのレコード一覧を表示します',
-			'data' => $data
+			'data' => $data,
+			'message' => $msg,
+			'bag' => $session->getFlashBag(),
+			'form' => $form->createView()
 		]);
   }
+
+	/**
+	 * @Route("/clear", name="clear")
+	 */
+	public function clear(Request $req, SessionInterface $session)
+	{
+		$session->getFlashBag()->clear();
+		return $this->redirect('/hello');
+	}
 
 	/**
   * @Route("/find", name="find")
@@ -166,5 +193,37 @@ class FindForm {
 
 	public function setFind($find) {
 		$this->find = $find;
+	}
+}
+
+class HelloForm
+{
+	private $name;
+	private $mail;
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+
+	public function getMail()
+	{
+		return $this->mail;
+	}
+
+	public function setMail($mail)
+	{
+		$this->mail = $mail;
+	}
+
+	// オーバーライド
+	public function __toString()
+	{
+		return '*** '.$this->name.'[' . $this->mail .'] ***';
 	}
 }
